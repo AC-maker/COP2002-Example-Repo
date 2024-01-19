7b0fe0411c74092268c64946cb65bb3c7998fd24
AC-maker <156834869+AC-maker@users.noreply.github.com>
2024-01-19 10:28:41 -0500

    Update Project_1.py

--- Project_1.py
+++ Project_1.py
@@ -1,6 +1,6 @@
->>> # Creating a project for GitHub
-... # By Alexandria Creel
-... 
-... print ("COP2002 Sprin 2024")
+# Creating a project for GitHub
+# By Alexandria Creel
+ 
+print ("COP2002 Sprin 2024")
 
->>> print ("I'm understanding how to use GitHub to keep track of changes!")
+print ("I'm understanding how to use GitHub to keep track of changes!")
