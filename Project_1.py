ccf50bbe9a90590cdbf855aa5e20e009b9f694a3
AC-maker <156834869+AC-maker@users.noreply.github.com>
2024-01-19 09:56:39 -0500

    Add files via upload

---
+++ Project_1.py
@@ -0,0 +1,15 @@
+Python 3.11.7 (tags/v3.11.7:fa7a6f2, Dec  4 2023, 19:24:49) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("Alexandria Creel")
+Alexandria Creel
+>>> print ("COP2002-0M2")
+COP2002-0M2
+>>> print ("Spring 2024")
+Spring 2024
+>>> print ("This is an interactive session!")
+This is an interactive session!
+>>> # Creating a project for GitHub
+... # By Alexandria Creel
+... 
+... print ("COP2002 Sprin 2024")
+COP2002 Sprin 2024
